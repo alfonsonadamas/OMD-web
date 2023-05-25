1fe95bacee840a41ff540073e642145dabefe4c6
Alfonso Gallardo <95257300+alfonsonadamas@users.noreply.github.com>
2023-05-24 22:39:59 -0600

    Cambios al enviar correo desde la pagina de incio

--- src/config/sendCustomEmail.js
+++ src/config/sendCustomEmail.js
@@ -1,15 +1,15 @@
 import { db } from "./firebase";
 import { collection, addDoc } from "firebase/firestore/lite";
 
-export default async function sendCustomEmail(email, subject, body) {
+export default async function sendCustomEmail(email, asunto, nombre) {
   const collectRef = collection(db, "mail");
 
   const emailContent = {
     to: email,
     message: {
-      subject: subject,
-      text: body,
-      html: `<p>${body}</p>`,
+      subject: "OMD te saluda",
+      text: `Hola ${nombre} nos comunicaremos muy pronto contigo a tu celular ${asunto} o a tu correo ${email}`,
+      html: `<p>Hola ${nombre} nos comunicaremos muy pronto contigo a tu celular ${asunto} o a tu correo ${email}</p>`,
     },
   };
 
